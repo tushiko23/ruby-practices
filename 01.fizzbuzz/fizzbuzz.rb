5f27cbb1140c1f26647dbfd0520d7cd53406724f
tushima <148033059+tushiko23@users.noreply.github.com>
2025-09-24 18:52:12 +0900

    3行目 %と==と&&について演算子の優先順位をわかるようにするために()を配置

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,7 +1,7 @@
 numbers = [*1..20]
 
 numbers.each do |number|
-  if number % 5 == 0 && number % 3 == 0
+  if ( ( number % 5 ) == 0 ) && ( ( number % 3 ) == 0 )
     puts "FizzBuzz"
   elsif number % 5 == 0
     puts "Buzz"
