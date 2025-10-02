c8706f57a9f0cf21eacf6b0b1a9066d240122537
tushima <148033059+tushiko23@users.noreply.github.com>
2025-10-03 05:29:27 +0900

    メソッド内のインデントをスペース2に修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,29 +9,29 @@ month = Date.today.month
 # program_calenderメソッドを定義。引数 year monthを指定。 
 def program_calendar(year,month)
 
-    # 変数から最初の月と最後の月を取得する 
-    first_day = Date.new(year, month, 1)
-    last_day = Date.new(year, month, -1)
+  # 変数から最初の月と最後の月を取得する 
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
 
-    # wdayメソッドで最初の月の日の曜日を数値で取得。（日:0,月:1,火:2)
-    first_wday = first_day.wday
+  # wdayメソッドで最初の月の日の曜日を数値で取得。（日:0,月:1,火:2)
+  first_wday = first_day.wday
 
-    # カレンダーの上部を表示　centerメソッドで幅が20で中央寄せ。
-    puts "#{month}月 #{year}".center(20," ")
-    puts "日 月 火 水 木 金 土"
+  # カレンダーの上部を表示　centerメソッドで幅が20で中央寄せ。
+  puts "#{month}月 #{year}".center(20," ")
+  puts "日 月 火 水 木 金 土"
 
-    # 初日の曜日まで空白を出力 曜日の値によって3スペースとfirst_wdayの数値で位置を決定。
-    print "   " * first_wday
+  # 初日の曜日まで空白を出力 曜日の値によって3スペースとfirst_wdayの数値で位置を決定。
+  print "   " * first_wday
 
-    # 各日付を繰り返し出力 date.saturday? 土曜日で改行
-    # .rjustメソッド(2)で幅2で右寄せ。
-    (first_day..last_day).each do |date|
-    print date.day.to_s.rjust(2) + " "
-    puts if date.saturday?
-    end
+  # 各日付を繰り返し出力 date.saturday? 土曜日で改行
+  # .rjustメソッド(2)で幅2で右寄せ。
+  (first_day..last_day).each do |date|
+  print date.day.to_s.rjust(2) + " "
+  puts if date.saturday?
+	end
 
-    # さらに改行してターミナルの字と重ならないようにする
-    puts
+# さらに改行してターミナルの字と重ならないようにする
+	puts
 
 end
 
