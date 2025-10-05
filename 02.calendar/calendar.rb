11329fd126fa15fa349c4cb829b787f530071197
tushima <148033059+tushiko23@users.noreply.github.com>
2025-10-06 06:47:25 +0900

    rubocopを導入し,インデントを修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,23 +6,22 @@ year = Date.today.year
 month = Date.today.month
 
 def show_calendar(year, month)
-
   first_day = Date.new(year, month, 1)
   last_day = Date.new(year, month, -1)
 
   first_wday = first_day.wday
 
-  puts "#{month}月 #{year}".center(20," ")
-  puts "日 月 火 水 木 金 土"
+  puts "#{month}月 #{year}".center(20, ' ')
+  puts '日 月 火 水 木 金 土'
 
-  print "   " * first_wday
+  print '   ' * first_wday
 
   (first_day..last_day).each do |date|
-    print date.day.to_s.rjust(2) + " "
+    print date.day.to_s.rjust(2) + ' '
     puts if date.saturday?
   end
 
-	puts	
+  puts
 end
 
 # -mで月を、-yで年を指定して表示できるようにする
@@ -30,14 +29,14 @@ end
 options = {}
 opt = OptionParser.new
 
-opt.on("-m month",Integer) do |month|
+opt.on('-m month', Integer) do |month|
   options[:month] = month
-end    
+end
 
-opt.on("-y year", Integer) do |year| 
-  options[:year] = year 
-end     
- 
+opt.on('-y year', Integer) do |year|
+  options[:year] = year
+end
+
 opt.parse!(ARGV)
 
 month = options[:month] || Date.today.month
@@ -46,8 +45,8 @@ year = options[:year] || Date.today.year
 
 # -m オプションに指定する引数が1~12以外の数値が入力された時にエラーメッセージの表示とプログラムの実行を終了するようにする
 if month < 1 || month >= 13
-  puts "-m オプションの指定は1~12のみ有効です"
+  puts '-m オプションの指定は1~12のみ有効です'
   exit
 end
 
-show_calendar(year,month)
+show_calendar(year, month)
